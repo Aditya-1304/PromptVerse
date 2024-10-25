e23a3943e250a6764f03e5f4b19ad30e91ff3f6e
Aditya Mandal <mandaladitya1614@gmail.com>
2024-10-25 20:39:51 +0530

    Fixed deployment issues

--- app/update-prompt/page.tsx
+++ app/update-prompt/page.tsx
@@ -1,73 +1,69 @@
-"use client"
+"use client";
 
 import { useEffect, useState, Suspense } from "react";
-import { useSession  } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
-
 import Form from "@components/Form";
 
-const EditPrompt = ()=>{
-
+const EditPrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const promptId = searchParams.get('id');
+  const promptId = searchParams.get("id");
 
-  const [submitting,setSubmitting] = useState(false);
-  const [post,setPost] = useState({
-    prompt:"",
-    tag:""
-  })
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
 
-  useEffect(()=>{
-    const getPromptsDetails = async ()=>{
+  useEffect(() => {
+    const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
 
       setPost({
         prompt: data.prompt,
-        tag: data.tag
-      })
-    }
-    if(promptId) getPromptsDetails();
-  },[promptId])
+        tag: data.tag,
+      });
+    };
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
-
-
-  const updatePrompt = async (e : any)=> {
+  const updatePrompt = async (e : any) => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert("Prompt Id not found");
+    if (!promptId) return alert("Prompt ID not found");
 
-    try{
+    try {
       const response = await fetch(`/api/prompt/${promptId}`, {
-        method : "PATCH",
-        body : JSON.stringify({
+        method: "PATCH",
+        body: JSON.stringify({
           prompt: post.prompt,
-          tag: post.tag
-        })
-      })
-      if(response.ok) {
-        router.push('/')
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
       }
-    }catch (e){
-      console.log(e);
-    }finally{
-      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
     }
-  }
+  };
 
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-    <Form 
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
     </Suspense>
-  )
-}
+  );
+};
 
 export default EditPrompt;
